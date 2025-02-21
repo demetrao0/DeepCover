8cf62020300ef5502fff47d4e4f35b5dc80223ee
demetrao0 <josepepeusa@icloud.com>
2025-02-21 03:23:07 -0600

    Tremendo Update
    Ya casi vamos bien

---
+++ Assets/Scripts/Botones/GameOver.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+        public string escenarioCambio;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Yes()
+    {
+        SceneManager.LoadScene(escenarioCambio);
+
+    
+    {
+
+
+    }
+
+}
+
+    public void No()
+    {
+
+        Debug.Log("Se cerro el juego");
+        Application.Quit();
+
+    }
+}
--- Assets/Scripts/Player/InteractivePhone.cs
+++ Assets/Scripts/Player/InteractivePhone.cs
@@ -1,30 +1,97 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InteractivePhone : MonoBehaviour
 {
 
-    public float timePased = 0f;
-    
+    public float tiempoTotal = 900f; // 15 minutos (900 segundos)
+    public float[] tiemposAviso = { 750f, 600f, 450f, 300f, 150f }; // Momentos donde suena la alarma
+    public AudioSource audioSource; // Referencia al AudioSource
+    public AudioClip[] sonidosAviso; // Sonidos específicos para cada aviso
+    public string escenaDestino = "EscenaFinal"; // Nombre de la escena a la que cambiar
+
+    private float tiempoRestante;
+    private int indiceAviso = 0;
+    private bool tiempoTerminado = false;
 
 
-    private void Update()
+
+    //--------------------------------- PREGUNTAS
+
+
+    public GameObject pregunta1;
+    public GameObject pregunta2;
+    public GameObject pregunta3;
+    public GameObject pregunta4;
+
+
+
+
+
+
+    void Start()
     {
-        timePased = timePased + Time.deltaTime;
+        tiempoRestante = tiempoTotal;
 
-
-
-
+        pregunta1.SetActive(false);
+        pregunta2.SetActive(false);
+        pregunta3.SetActive(false);
+        pregunta4.SetActive(false);
     }
 
-    public void ActivarObjeto()
+    void Update()
     {
+        if (tiempoRestante > 0)
+        {
+            tiempoRestante -= Time.deltaTime;
 
+            // Comprobamos si hemos llegado al siguiente aviso
+            if (indiceAviso < tiemposAviso.Length && tiempoRestante <= tiemposAviso[indiceAviso])
+            {
+                ActivarAviso(indiceAviso);
+                indiceAviso++; // Pasamos al siguiente aviso
+            }
+        }
+        else if (!tiempoTerminado) // Cuando el tiempo llega a 0
+        {
+            tiempoTerminado = true;
+            CambiarEscena();
+        }
+    }
 
-        //gameObject.transform.position = spaawn.transform.position;
+    void ActivarAviso(int indice)
+    {
+        Debug.Log("Alarma " + (indice + 1) + " activada en " + tiempoRestante + " segundos");
 
-        //Player.GetComponent<PlayerMovement>().LLaveOn();
+        // Reproducir sonido específico para cada aviso
+        if (indice < sonidosAviso.Length && sonidosAviso[indice] != null)
+        {
+            audioSource.clip = sonidosAviso[indice];
+            audioSource.Play();
+        }
 
+        // Aquí puedes agregar el comportamiento único de cada aviso
+        switch (indice)
+        {
+            case 0:
+                pregunta1.SetActive(true);
+                break;
+            case 1:
+                pregunta2.SetActive(true);
+                break;
+            case 2:
+                pregunta3.SetActive(true);
+                break;
+            case 3:
+                pregunta4.SetActive(true);
+                break;
+           
+        }
+    }
 
-        Destroy(gameObject);
+    void CambiarEscena()
+    {
+        Debug.Log("Tiempo finalizado. Cambiando a la escena: " + escenaDestino);
+        SceneManager.LoadScene(escenaDestino);
     }
 }
